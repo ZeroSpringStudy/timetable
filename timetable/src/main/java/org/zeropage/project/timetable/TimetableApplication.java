70c3974c43f26d9158889b0bcd8b0a41d8d61f36
cau20232907 <hs20232907@cau.ac.kr>
2024-07-20 23:29:23 +0900

    BaseProject

---
+++ timetable/src/main/java/org/zeropage/project/timetable/TimetableApplication.java
@@ -0,0 +1,13 @@
+package org.zeropage.project.timetable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimetableApplication.class, args);
+	}
+
+}
